0e66108b55b4e0ce8ac5d3cafb081277965651a4
fabiantheblind <fabiantheblind@cb82aaa4-aae0-7e99-7a62-07f142bb5cd5>
2010-05-07 19:36:09 +0000

    Added some Text. Tried the processing nexttext librarie but it throws an nullointerexception…

--- trunk/src/geo/PlaceMarker.java
+++ trunk/src/geo/PlaceMarker.java
@@ -2,7 +2,10 @@ package geo;
 
 
 
+
+import net.nexttext.Book;
 import processing.core.PApplet;
+import processing.core.PFont;
 
 public class PlaceMarker {
 	 PApplet p;
@@ -10,6 +13,8 @@ public class PlaceMarker {
 	  public float lat;
 	  public float lng;
 
+	 
+	  
 	  public PlaceMarker(PApplet CP,String name, float lat, float lng) {
 	    this.name = name;
 	    this.lat = lat;
@@ -18,21 +23,38 @@ public class PlaceMarker {
 
 	  }
 
+@SuppressWarnings("static-access")
+public void display(PFont font) {
+	p.smooth();
 
-//@SuppressWarnings("static-access")
-public void display() {
-		
 	    // Equirectangular projection
 //	p.println("in method display call lat lon");
 	    float x= p.map(lng, -180, 180, 0, p.width); 
 	    float y= p.map(lat, 90, -90, 0, p.height);
-	    
+	    p.textFont(font); 
+
 	    p.noStroke();
-	    p.fill(255, 0, 0, 50);
-	    p.ellipse(x, y, 15, 15);
-	    p.fill(255, 0, 0, 200);
+	    p.fill(230, 50);
+	    p.ellipse(x, y, 10, 10);
+	    p.fill(128, 200);
 	    p.ellipse(x, y, 5, 5);
-//	    p.rect(0, 0, p.width, p.height);
+	    p.fill(255,230);
+	    p.text(name,x,y);
+//	    try {
+//	    	p.smooth();
+//			book.addText(name, p.floor(x), p.floor(y));
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	      try {
+//			book.stepAndDraw();
+//;
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
 	  }
 
 	public  String toString() {
