32642e39919cd70f74b2368cb6becb09fd0d9ae5
fabiantheblind <fabiantheblind@cb82aaa4-aae0-7e99-7a62-07f142bb5cd5>
2010-05-07 13:01:40 +0000

    Some System.out.println()'s to see where u does what…

--- src/geo/PlaceMarker.java
+++ src/geo/PlaceMarker.java
@@ -1,33 +1,38 @@
 package geo;
 
+
+
 import processing.core.PApplet;
 
 public class PlaceMarker {
 	public PApplet p;
 	  String name;
-	  float lat;
-	  float lng;
+	  public float lat;
+	  public float lng;
 
 	  public PlaceMarker(String name, float lat, float lng) {
 	    this.name = name;
 	    this.lat = lat;
 	    this.lng = lng;
-
+//	    this.p = p;
 
 	  }
 
 
-@SuppressWarnings("static-access")
+//@SuppressWarnings("static-access")
 public void display() {
+		
 	    // Equirectangular projection
-	    float x = p.map(lng, -180, 180, 0, p.width); 
-	    float y = p.map(lat, 90, -90, 0, p.height);
+	p.println("in method display call lat lon");
+//	    float x= p.map(lng, -180, 180, 0, p.width); 
+//	    float y= p.map(lat, 90, -90, 0, p.height);
 	    
 	    p.noStroke();
 	    p.fill(255, 0, 0, 50);
-	    p.ellipse(x, y, 15, 15);
-	    p.fill(255, 0, 0, 200);
-	    p.ellipse(x, y, 5, 5);
+//	    p.ellipse(x, y, 15, 15);
+//	    p.fill(255, 0, 0, 200);
+//	    p.ellipse(x, y, 5, 5);
+	    p.rect(0, 0, p.width, p.height);
 	  }
 
 	public  String toString() {
