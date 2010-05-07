0d375086f0ce672dc358fc7f9120955ad20f3972
fabiantheblind <fabiantheblind@cb82aaa4-aae0-7e99-7a62-07f142bb5cd5>
2010-05-07 13:54:51 +0000

    Now it works and Displays the Geo Pos of all Places in the Description of the RssFeed

--- src/geo/PlaceMarker.java
+++ src/geo/PlaceMarker.java
@@ -5,16 +5,16 @@ package geo;
 import processing.core.PApplet;
 
 public class PlaceMarker {
-	public PApplet p;
+	 PApplet p;
 	  String name;
 	  public float lat;
 	  public float lng;
 
-	  public PlaceMarker(String name, float lat, float lng) {
+	  public PlaceMarker(PApplet CP,String name, float lat, float lng) {
 	    this.name = name;
 	    this.lat = lat;
 	    this.lng = lng;
-//	    this.p = p;
+	    p = CP;
 
 	  }
 
@@ -23,16 +23,16 @@ public class PlaceMarker {
 public void display() {
 		
 	    // Equirectangular projection
-	p.println("in method display call lat lon");
-//	    float x= p.map(lng, -180, 180, 0, p.width); 
-//	    float y= p.map(lat, 90, -90, 0, p.height);
+//	p.println("in method display call lat lon");
+	    float x= p.map(lng, -180, 180, 0, p.width); 
+	    float y= p.map(lat, 90, -90, 0, p.height);
 	    
 	    p.noStroke();
 	    p.fill(255, 0, 0, 50);
-//	    p.ellipse(x, y, 15, 15);
-//	    p.fill(255, 0, 0, 200);
-//	    p.ellipse(x, y, 5, 5);
-	    p.rect(0, 0, p.width, p.height);
+	    p.ellipse(x, y, 15, 15);
+	    p.fill(255, 0, 0, 200);
+	    p.ellipse(x, y, 5, 5);
+//	    p.rect(0, 0, p.width, p.height);
 	  }
 
 	public  String toString() {
