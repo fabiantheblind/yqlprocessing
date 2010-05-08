67afb11c21ba6740ac611242049c6aa406fcb46e
fabiantheblind <fabiantheblind@cb82aaa4-aae0-7e99-7a62-07f142bb5cd5>
2010-05-08 09:33:54 +0000

    Cleaned The Code

--- trunk/src/main/Main.java
+++ trunk/src/main/Main.java
@@ -10,13 +10,14 @@
 
 package main;
 import geo.PlacemarkerManager;
-import java.io.UnsupportedEncodingException;
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.xml.XMLElement;
 import util.HTMLEncoding;
+import util.Querys;
 import util.Style;
 import util.XmlWriter;
+import util.YQLStatements;
 
 
 
@@ -27,92 +28,51 @@ public class Main extends PApplet {
 	 */
 	private static final long serialVersionUID = -1643689331016244574L;
 	
-//	public ArrayList<PlaceMarker> placeMarkersList;
-//	public ArrayList placeMarkersList = new ArrayList();
-	private String keyword = "Mouse";
-	public String myRssQueryUncoded = "select * from rss where url='http://www.the-moron.net/blog/?feed=rss2'";
-	public  String myFlickrQueryUncoded = "select * from flickr.photos.search where text = '" + keyword + "' limit 9";
-	public String myUnderTheRadarMediaUncoded ="select * from rss where url='http://feeds.delicious.com/v2/rss/maasanova'";
 
-	public String myWholeQuery;
-	public	String myNextWholeQuery;
-	public String yqlApiUrl = "http://query.yahooapis.com/v1/public/yql";
 	public XMLElement xmlResponse;
 	public XMLElement xmlNextResponse;
 
 	public PlacemarkerManager pmManager;
 	public HTMLEncoding coder = new HTMLEncoding();
 	public XmlWriter xmlWriter;
-	public PApplet  p;
 	PImage worldMap;
-//	public PFont font;
-	
+	public Querys rssQuery = new Querys(this);
+	public Querys placemakerQuery = new Querys(this);
+
 	public void setup() {
-		
+		pmManager = new PlacemarkerManager(this);
 		Style.setPApplet(this);
+		YQLStatements.setPApplet(this);
 		Style.create();
-
-//		font = createFont("./data/GenBasR.ttf", 10f);
-
 		worldMap = loadImage("./data/worldmap-equirectangular-s-sw.png");
 		size(1200,600);
-
-		pmManager = new PlacemarkerManager(this);
 		
-		try {
-		myWholeQuery = yqlApiUrl + "?q=" + coder.encode(myUnderTheRadarMediaUncoded) + "&amp;format=xml";
-		println("ENcoding Query RSS");
-		} catch (UnsupportedEncodingException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		println("Sorry an error ocurred during encoding the string");
-		}
-		xmlResponse = new XMLElement(this, myWholeQuery);
+		
+		xmlResponse = rssQuery.getResponseFromTable(YQLStatements.myRssQueryUncoded);
 		println("got Response from RSS");
 		XMLElement[] descrXMLElements = xmlResponse.getChildren("results/item/description");
 		println("XMLElements >discription> collected from RSS");
-		String	 myPlaceQueryUncoded;
 		String myPlaceKey;
-		
 		println("look for Placemarks via placemaker");
+		
 		for(int i =0;i<descrXMLElements.length;i++){
-			 
 //			xmlWriter.makeXmlFile("myPlaceMarkFile"+i);
-			 myPlaceKey = descrXMLElements[i].getContent();	
-			myPlaceQueryUncoded="SELECT * FROM geo.placemaker WHERE documentContent = \""
-			+myPlaceKey
-			+"\" AND documentType=\"text/plain\"";
-					
-			try {
-			myNextWholeQuery = yqlApiUrl + "?q=" + coder.encode(myPlaceQueryUncoded) + "&amp;format=xml";
-	
-			} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			println("Sorry an error ocurred during encoding the string");
-			}
-			xmlNextResponse = new XMLElement(this, myNextWholeQuery);	
+			myPlaceKey = descrXMLElements[i].getContent();				
+			xmlNextResponse = placemakerQuery.getResponseFromTable(YQLStatements.makePlacesStat(myPlaceKey));	
 			println("Got Response from placemaker");
 			XMLElement[] finalPlaceXMLElements = xmlNextResponse.getChildren("results/matches/match/place");
 			println("XMLElements <place> collected");
-
-					  for(int j =0;j<finalPlaceXMLElements.length;j++){
-						  pmManager.init(finalPlaceXMLElements);	
-					  	}					  
-//						xmlWriter.writeXml(xmlNextResponse);
-						println("Got all Placemarker in Arraylist");
+			pmManager.init(finalPlaceXMLElements);	
+//			xmlWriter.writeXml(xmlNextResponse);
+			println("Got all Placemarker in Arraylist");
 		 }
 	}
 
 	public void draw () {
 	
 		smooth();
-		 image(worldMap, 0, 0, worldMap.width, worldMap.height);
-//			background(0);
-//		println("Call PlacemarkerManager Method drawPlaces()");
+		image(worldMap, 0, 0, worldMap.width, worldMap.height);
 		pmManager.drawPlaces();
-//		fill(0);
-//		rect(0,0,width,height);
 	
 	}
 	
